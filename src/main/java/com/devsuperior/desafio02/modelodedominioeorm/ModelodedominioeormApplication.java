d24c9687cc94c81027dd6f7c0a7c82db4f281a65
Junior Bortolanza <gbortolanzajr@gmail.com>
2025-09-10 07:57:42 -0300

    Project created

---
+++ src/main/java/com/devsuperior/desafio02/modelodedominioeorm/ModelodedominioeormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio02.modelodedominioeorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelodedominioeormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelodedominioeormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio02/modelodedominioeorm/ModelodedominioeormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio02.modelodedominioeorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelodedominioeormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
